05ff3a802970f1f2ff9ed4497a7a562f0d1bb94f
Daniel Blankenship <danielblankenship@Daniels-MacBook-Air.local>
2016-09-12 18:37:41 -0600

    added api changes

--- api/index.php
+++ api/index.php
@@ -1,9 +1,10 @@
-<?php
-require 'Api.php';
-
-try {
-   $api = new Api($_GET);
-} catch (Exception $e) {
-   echo $e->getMessage();
-}
-?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>My Api</title>
+   </head>
+   <body>
+      <h1>My Api</h1>
+   </body>
+</html>
---
+++ api/index.php.bak
@@ -0,0 +1,9 @@
+<?php
+require 'Api.php';
+
+try {
+   $api = new Api($_GET);
+} catch (Exception $e) {
+   echo $e->getMessage();
+}
+?>
