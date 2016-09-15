aea9ac8d632978aa3cb246f7490b97508fe52003
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-09-15 09:05:54 -0600

    added additional comments to Api.class.php

--- api/Api.class.php
+++ api/Api.class.php
@@ -2,38 +2,43 @@
 abstract class Api {
 
 	/**
-     * Property: method
-     * The HTTP method this request was made in, either GET, POST, PUT or DELETE
-     */
+	 * The HTTP method this request was made in, either GET, POST, PUT or DELETE
+	 * @var string
+	 */
     protected $method = '';
 
 	/**
-     * Property: endpoint
-     * The Model requested in the URI. eg: /files
-     */
+	 * The Model requested in the URI. eg: /files
+	 * @var string
+	 */
     protected $endpoint = '';
 
 	/**
-     * Property: verb
-     * An optional additional descriptor about the endpoint, used for things that can
+	 * An optional additional descriptor about the endpoint, used for things that can
      * not be handled by the basic methods. eg: /files/process
-     */
+	 * @var string
+	 */
     protected $verb = '';
 
 	/**
-     * Property: args
-     * Any additional URI components after the endpoint and verb have been removed, in our
+	 * Any additional URI components after the endpoint and verb have been removed, in our
      * case, an integer ID for the resource. eg: /<endpoint>/<verb>/<arg0>/<arg1>
      * or /<endpoint>/<arg0>
-     */
+	 * @var Array
+	 */
     protected $args = Array();
 
 	/**
-     * Property: file
-     * Stores the input of the PUT request
-     */
+	 * Stores the input of the PUT request
+	 * @var [type]
+	 */
     protected $file = Null;
 
+	/**
+	 * Trims extra whitespace, and strip tags from URI data.
+	 * @param  Array  $data URI request data
+	 * @return Array        cleaned URI request data
+	 */
 	private function _cleanInputs($data) {
         $clean_input = Array();
 
@@ -49,6 +54,11 @@ abstract class Api {
         return $clean_input;
     }
 
+	/**
+	 * Returns an error status based on an error code.
+	 * @param  integer $code an HTTP status code (ie. 200)
+	 * @return string        the associated message
+	 */
 	private function _requestStatus($code) {
 	    $status = array(
 	        200 => 'OK',
@@ -60,12 +70,22 @@ abstract class Api {
 	    return ($status[$code]) ? $status[$code] : $status[500];
 	}
 
+	/**
+	 * Sets the header to the current HTTP status.
+	 * @param  Object  $data   data returned from the model
+	 * @param  integer $status an HTTP status code (200 by default)
+	 * @return JSON
+	 */
     private function _response($data, $status = 200) {
         header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
 
         return json_encode($data);
     }
 
+	/**
+	 * Sanitizes the URI request data.
+	 * @param  string $method the type of request made (ie. HTTP GET)
+	 */
 	private function _sanitizeInputs($method) {
 		switch($method) {
 			case 'DELETE':
@@ -85,10 +105,21 @@ abstract class Api {
 		}
 	}
 
+	/**
+	 * Sets the value of args to any extra request data that isn't the
+	 *  endpoint or the verb. This data will then be passed into the endpoint
+	 *  as a function parameter.
+	 * @param [type] $request [description]
+	 */
 	private function _setArgs($request) {
 		$this->args = $request;
 	}
 
+	/**
+	 * Sets the value of the endpoint. This is the name of the model we will be
+	 *  receiving data from.
+	 * @param Array $request the URI request data
+	 */
 	private function _setEndpoint(&$request) {
 		if (isset($request['endpoint'])) {
 			$this->endpoint = $request['endpoint'];
@@ -98,6 +129,10 @@ abstract class Api {
 		}
 	}
 
+	/**
+	 * Sets the method of the request (ie. HTTP GET).
+	 * @param string $requestMethod the HTTP request method (ie. HTTP GET)
+	 */
 	private function _setMethod($requestMethod) {
 		$this->method = $requestMethod;
 
@@ -114,6 +149,11 @@ abstract class Api {
 		}
 	}
 
+	/**
+	 * Sets the extra description of how to process the request. For example,
+	 *  /files/process.
+	 * @param Array $request the URI request data
+	 */
 	private function _setVerb(&$request) {
 		if (isset($request['verb'])) {
 			$this->verb = $request['verb'];
@@ -121,6 +161,10 @@ abstract class Api {
 		}
 	}
 
+	/**
+	 * Strips any request that begins with _ from the request URI array.
+	 * @param  Array $request the URI request data
+	 */
 	private function _stripUnderscoreRequests(&$request) {
 		foreach ($request as $key => $val) {
 			if (substr($key, 0, 1) == '_') {
@@ -146,6 +190,12 @@ abstract class Api {
 		$this->_sanitizeInputs($this->method);
     }
 
+	/**
+	 * Calls the endpoint with any arguments passed in. If the endpoint doesn't
+	 *  exist, it returns a 404 (Not found) status.
+	 * @return JSON || string either returns the JSON encoded data or an HTTP
+	 *                 status message
+	 */
    	public function processAPI() {
         if (method_exists($this, $this->endpoint)) {
             return $this->_response($this->{$this->endpoint}($this->args));
