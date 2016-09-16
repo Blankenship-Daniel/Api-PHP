747e34cc5d793631d852dd4274a4542c817b3afc
Daniel Blankenship <danielblankenship@Daniels-MacBook-Air.local>
2016-09-15 19:01:27 -0600

    added vendor autoload to header

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
          $app = new Silex\Application();
          $app['debug'] = true;
 
+         $dbopts = parse_url(getenv('DATABASE_URL'));
+
          $app->register(new Herrera\Pdo\PdoServiceProvider(),
             array(
                'pdo.dsn'      => 'pgsql:dbname=' .
