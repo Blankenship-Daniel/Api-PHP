7828416d66d7d73a8631f6edb1c798fed94b9248
Daniel Blankenship <danielblankenship@Daniels-MacBook-Air.local>
2016-09-15 18:46:38 -0600

    new commit

--- index.php
+++ index.php
@@ -7,17 +7,16 @@
    <body>
       <h1>Hello World</h1>
       <?php
-         $dbopts = parse_url(getenv('DATABASE_URL'));
-         // $app->register(new Herrera\Pdo\PdoServiceProvider(),
-         //    array(
-         //       'pdo.dsn'      => 'pgsql:dbname=' .
-         //                         ltrim($dbopts["path"],'/') .
-         //                         ';host='.$dbopts["host"] .
-         //                         ';port=' . $dbopts["port"],
-         //       'pdo.username' => $dbopts["user"],
-         //       'pdo.password' => $dbopts["pass"]
-         //    )
-         // );
+         $app->register(new Herrera\Pdo\PdoServiceProvider(),
+            array(
+               'pdo.dsn'      => 'pgsql:dbname=' .
+                                 ltrim($dbopts["path"],'/') .
+                                 ';host='.$dbopts["host"] .
+                                 ';port=' . $dbopts["port"],
+               'pdo.username' => $dbopts["user"],
+               'pdo.password' => $dbopts["pass"]
+            )
+         );
 
          echo '<pre>';
          var_dump($dbopts);
