f0525c80d354969d0f00628003f54283f246348a
Daniel Blankenship <danielblankenship@Daniels-MacBook-Air.local>
2016-09-19 19:33:42 -0600

    added jquery and changed to bower

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 
       <link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
       <script type="text/javascript" src="bower_components/jQuery/dist/jquery.min.js"></script>
-      <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
    </head>
    <body>
       <h1>Hello World</h1>
@@ -16,4 +15,5 @@
          <div class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
       </div>
    </body>
+   <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 </html>
