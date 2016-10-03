eaa327b4f16d0f47461676aeaa160c3640d98466
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-03 14:58:38 -0600

    updated repo

--- db/dbConnect.php
+++ db/dbConnect.php
@@ -1,13 +1,2 @@
 <?php
-$db = new Silex\Application();
-$db['debug'] = true;
-
-$dbopts = parse_url(getenv('DATABASE_URL'));
-
-$db->register(new Herrera\Pdo\PdoServiceProvider(), array(
-      'pdo.dsn' => 'pgsql:dbname=' . ltrim($dbopts["path"],'/') . ';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-      'pdo.username' => $dbopts["user"],
-      'pdo.password' => $dbopts["pass"]
-   )
-);
 ?>
