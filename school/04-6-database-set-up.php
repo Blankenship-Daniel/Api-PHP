0abe078237821e13ffd4649277ecf134405f4882
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-03 15:30:04 -0600

    added new assignment 4.6

---
+++ school/04-6-database-set-up.php
@@ -0,0 +1,9 @@
+<?php require_once('../layouts/header.php') ?>
+<?php require_once('../db/dbConnect.php') ?>
+
+<div class="container">
+    <h1>Database Setup</h1>
+    <?php var_dump($conn) ?>
+</div>
+
+<?php require_once('../layouts/footer.php') ?>
