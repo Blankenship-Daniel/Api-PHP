7e47af2bc294c843ef522eb50b186b8d1be02899
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-11 17:56:56 -0600

    added DBPantry class

--- school/05-6-php-database-access.php
+++ school/05-6-php-database-access.php
@@ -5,15 +5,16 @@
 <div class="container">
     <h1>Database Setup</h1>
     <div class="boxed">
+        <select id="food_types">
         <?php
             $db = new DBPantry($conn);
             $food_types = $db->getAllFoodTypes();
 
             foreach ($food_types as $food_type) {
-                echo $food_type["id"];
-                echo $food_type["name"];
+                echo "<option value='" . $food_type['id'] . "'>" . $food_type['name'] . "</option>";
             }
         ?>
+        </select>
     </div>
 </div>
 
