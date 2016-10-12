a3e147c25bea31ea08e04ba05a969ba8756b3118
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-11 20:18:25 -0600

    added DBPantry class

--- school/05-6-php-database-access.php
+++ school/05-6-php-database-access.php
@@ -4,37 +4,45 @@
 
 <div class="container">
     <h1>Database Setup</h1>
-    <div class="boxed">
-        <label for="food_types">Food types: </label>
-        <select onchange="getFood(this.value)" name="food_types" id="food_types">
-        <?php
-            $db = new DBPantry($conn);
-            $food_types = $db->getAllFoodTypes();
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-6">
+                <div class="boxed">
+                    <label for="food_types">Food types: </label>
+                    <select onchange="getFood(this.value)" name="food_types" id="food_types">
+                    <?php
+                        $db = new DBPantry($conn);
+                        $food_types = $db->getAllFoodTypes();
 
-            foreach ($food_types as $food_type) {
-                echo "<option value='" . $food_type['id'] . "'>" .
-                    strtolower(str_replace("_", " ", $food_type['name'])) .
-                    "</option>";
-            }
+                        foreach ($food_types as $food_type) {
+                            echo "<option value='" . $food_type['id'] . "'>" .
+                                strtolower(str_replace("_", " ", $food_type['name'])) .
+                                "</option>";
+                        }
 
-            mysqli_close($conn);
-        ?>
-        </select>
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>
-                        Food Name
-                    </th>
-                    <th>
-                        Expiration Date
-                    </th>
-                </tr>
-            </thead>
-            <tbody id="foods">
-
-            </tbody>
-        </table>
+                        mysqli_close($conn);
+                    ?>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="boxed">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Food Name
+                                </th>
+                                <th>
+                                    Expiration Date
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody id="foods"></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
