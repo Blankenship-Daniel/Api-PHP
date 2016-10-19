71b2cc5d685dda260490f3c07d7a980d1c06597d
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-18 19:10:37 -0600

    Adding assignment 6.6

---
+++ school/06-6-php-database-modification.php
@@ -0,0 +1,49 @@
+<?php require_once('../layouts/header.php') ?>
+<?php require_once('../db/dbConnect.php') ?>
+<?php require_once('../db/DBPantry.php') ?>
+
+<div class="container">
+    <h1>PHP Database Access</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-6">
+                <div class="boxed">
+                    <label for="food_types">Food types: </label>
+                    <select onchange="getFood(this.value)" name="food_types" id="food_types">
+                    <?php
+                        $db = new DBPantry($conn);
+                        $food_types = $db->getAllFoodTypes();
+
+                        foreach ($food_types as $food_type) {
+                            echo "<option value='" . $food_type['id'] . "'>" .
+                                strtolower(str_replace("_", " ", $food_type['name'])) .
+                                "</option>";
+                        }
+
+                        mysqli_close($conn);
+                    ?>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="boxed">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    Food Name
+                                </th>
+                                <th>
+                                    Expiration Date
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody id="foods"></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('../layouts/footer.php') ?>
