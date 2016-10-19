754f52fdbf4b95f7894bb8d629b8dddd6fa38940
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-18 20:03:37 -0600

    Testing DB

--- db/DBPantry.php
+++ db/DBPantry.php
@@ -12,9 +12,7 @@ class DBPantry {
         $stmt->execute();
         $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
             return $result->fetch_all(MYSQLI_ASSOC);
-        }
 
         return false;
     }
