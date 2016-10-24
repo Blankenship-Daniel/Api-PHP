1d7ea9fc2570b0a66a89e4443fddcfcaf13cf9fe
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-24 10:43:29 -0600

    Added email and password inputs

--- school/login.php
+++ school/login.php
@@ -5,7 +5,14 @@
 <div class="container">
     <h1>User Login</h1>
     <div class="row">
-
+        <div class="col-md-12">
+            <div class="boxed form-group">
+                <label for="email">Email: </label>
+                <input type="text" name="email" placeholder="john@doe.com" class="form-control">
+                <label for="password">Password: </label>
+                <input type="password" name="password" class="form-control">
+            </div>
+        </div>
     </div>
 </div>
 
