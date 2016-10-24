8acc9c99a2885e6b3a59495e01aae0566e62f896
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-24 12:52:57 -0600

    Fixed jquery validate

--- layouts/footer.php
+++ layouts/footer.php
@@ -4,6 +4,6 @@
 </body>
 <script type="text/javascript" src="/bower_components/jquery/dist/jquery.min.js"></script>
 <script type="text/javascript" src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="/bower_components/jquery-validation/jquery.validate.min.js"></script>
+<script type="text/javascript" src="/bower_components/jquery-validation/dist/jquery.validate.min.js"></script>
 <script type="text/javascript" src="/js/app.js"></script>
 </html>
